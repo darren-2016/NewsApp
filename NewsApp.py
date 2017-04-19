b203c3d0323905f3b9e220d29ab2dd8b6a65bc29
darren-2016 <darren.draper@gpsengine.net>
2017-04-19 22:34:55 +1000

    Added BBC News feed

--- NewsApp.py
+++ NewsApp.py
@@ -1,2 +1,25 @@
 print "NewsApp"
 
+import requests
+import json
+
+
+def News():
+    r = requests.get("https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=a75fa57d45b045f8857f6c2ee117dff6")
+
+    print r.status_code
+
+    #print r.text
+
+    jsonResponse = json.loads(json.dumps(r.json(), indent=2))
+
+    #print "JSON Response:"
+    #print json.dumps(r.json(), indent=2)
+
+    print "Headlines"
+    print "========="
+    print
+    for message in jsonResponse['articles']:
+        print message['description']
+        print
+News()
